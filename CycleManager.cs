d7211a8b1fd8cb71485b55ce05813cec80b1fd88
savinProject402 <74914774+savinProject402@users.noreply.github.com>
2021-03-02 04:46:34 +0200

    Add files via upload

---
+++ CycleManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicSum
+{
+    public class CycleManager
+    {
+        public int GetEvenCount(int limit)
+        {
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz " + i);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz " + i);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz " + i);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return limit;
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LogicSum;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("FizzBuzz - project");
+
+            Console.WriteLine("Insert first number");
+
+            int currentValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert second number");
+            int limit = int.Parse(Console.ReadLine());
+
+            CycleManager maneger = new CycleManager();
+            currentValue = maneger.GetEvenCount(limit);
+
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
